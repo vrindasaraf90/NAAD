0c46d32a3628fd451fc49fee43c098926a612b6b
vrindasaraf90 <72187181+vrindasaraf90@users.noreply.github.com>
2021-03-31 21:34:58 +0530

    Add files via upload

---
+++ src/com/Introduction/StringTOInt.java
@@ -0,0 +1,15 @@
+package com.TypeConversion;
+
+public class StringTOInt {
+    public static void main(String[] args){
+
+        String str1 = "123";
+        String str2 = "9990";
+
+        int number1 = Integer.parseInt(str1);
+        int number2 = Integer.parseInt(str2);
+
+        System.out.println(number1);
+        System.out.println(number2);
+    }
+}
